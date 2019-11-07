cee1a2e4a2a3ab68cf466e44936789c2e3c67a65
Pedro Chaves Freire <pedro.chaves.freire@avanade.com>
2019-11-07 08:16:15 -0300

    componentização do navBar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,22 +3,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterModule } from "@angular/router";
 
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 import { InMemoryDataBase } from "../in-memory-database";
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataBase)
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataBase),
+    RouterModule
   ],
   exports:[
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+
+    //shared components
+    NavbarComponent
   ]
 })
 export class CoreModule { }
